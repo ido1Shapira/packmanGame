c1dcac92b5c41469f98c3d420394b60cbfc2ad77
Ido <idshapira051@gmail.com>
2022-05-08 22:28:34 +0300

    setup env

--- python_code/setup.py
+++ python_code/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_packman',
   version='1.0',
-  install_requires=['gym', 'tensorflow>=2.6', 'matplotlib', 'IPython', 'opencv-python']  # And any other dependencies Env needs
+  install_requires=['gym==0.21.0', 'tensorflow==2.6', 'matplotlib', 'IPython', 'opencv-python'],  # And any other dependencies Env needs
+  packages=["gym_packman"]
   )
