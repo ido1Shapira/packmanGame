89e3d4ed352c40e50960e33f0c26d4a4dbc593f8
Ido <idshapira051@gmail.com>
2022-05-13 10:16:32 +0300

    dqn model with two different human models

--- python_code/gym_packman/envs/Packman_env.py
+++ python_code/gym_packman/envs/Packman_env.py
@@ -61,11 +61,10 @@ class PackmanEnv(Env):
         self.state = None
 
         # Load human model from the computer
-        self.human_model = tf.keras.models.load_model('./data/'+self.map_dir+'/humanModel_v0_dropout.h5')
-        # self.num_of_models = 2
-        # self.human_models = []
-        # for i in range(self.num_of_models):
-            # self.human_models.append(tf.keras.models.load_model('./data/'+self.map_dir+'/humanModel_'+str(i)+'_v0.h5'))
+        self.num_of_models = 2
+        self.human_models = []
+        for i in range(self.num_of_models):
+            self.human_models.append(tf.keras.models.load_model('./data/'+self.map_dir+'/humanModel_'+str(i)+'_v1.h5'))
         
         self.sample_from_distribution = True
 
@@ -366,8 +365,8 @@ class PackmanEnv(Env):
         
         actions = []
         # for human_model in self.human_models:
-        for _ in range(2):
-            predictions = self.human_model.predict(img_array)
+        for model in self.human_models:
+            predictions = model.predict(img_array)
             score = predictions[0]
             # score = score[0]
             
